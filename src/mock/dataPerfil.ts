1b92472cd20283c435d9375d4eed8f7211ccedec
Adriano Wiedmann <adriano.ulrich10@gmail.com>
2025-06-24 02:02:25 -0300

    feat: adiciona arquivos inicias para tela perfil

---
+++ src/mock/dataPerfil.ts
@@ -0,0 +1,26 @@
+export const mockUser = {
+  id: "1",
+  nome: "Rodrigues da Silva",
+  curso: "Ciência da Computação",
+  departamento: "Dept. Ciência da Computação",
+  email: "rodrigues@gmail.com",
+  avatarUrl: "/default-avatar.png",
+  avaliacoes: [
+    {
+      id: 1,
+      data: "17/04/2024",
+      professor: "Cleiton",
+      disciplina: "culinaria 1",
+      conteudo: "Lorem ipsum...",
+      comentarios: 2,
+    },
+    {
+      id: 2,
+      data: "15/04/2024",
+      professor: "Jorge",
+      disciplina: "Historia 1",
+      conteudo: "Lorem ipsum...",
+      comentarios: 5,
+    },
+  ],
+};
